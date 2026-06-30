d24013db9dc1ffdb93f02da4c7be4fd6e665cfc3
Chooben <benmchoo@gmail.com>
2026-06-30 12:02:49 -0400

    Setup waste log script with sql table

--- scripts/script.py
+++ scripts/script.py
@@ -5,7 +5,22 @@ from sqlalchemy import create_engine, text
 
 days_order = ["Monday", "Tuesday", "Wednesday", "Thursday","Friday", "Saturday", "Sunday"]
 
-DATABASE_URL = os.environ["DATABASE_URL"]
-SHEETS_URL = os.environ["GOOGLE_SHEET"]
+load_dotenv()
 
-print("Script is running")
+DATABASE_URL = os.environ["DATABASE_URL"]
+#SHEETS_URL = os.environ["GOOGLE_SHEET"]
+
+engine = create_engine(DATABASE_URL)
+
+with engine.connect() as conn:
+    df = pd.read_sql(text("""
+        SELECT w.*, p.category, p.core
+        FROM daily_waste w
+        LEFT JOIN pastries p ON w.name = p.name
+    """), conn)
+
+df["date"] = pd.to_datetime(df["date"])
+df["day"] =  pd.Categorical(df["date"].dt.day_name(), categories=days_order, ordered=True)
+df["week_num"] = (df["date"] - df["date"].min()).dt.days //7 + 1
+
+print(df)
