441092e4822d6cf58071645ea6bbcdeeae0f9ab0
Chooben <benmchoo@gmail.com>
2026-06-30 14:29:17 -0400

    Initial script set to sync DB daily

--- scripts/script.py
+++ scripts/script.py
@@ -8,19 +8,42 @@ days_order = ["Monday", "Tuesday", "Wednesday", "Thursday","Friday", "Saturday",
 load_dotenv()
 
 DATABASE_URL = os.environ["DATABASE_URL"]
-#SHEETS_URL = os.environ["GOOGLE_SHEET"]
+SHEETS_URL = os.environ["GOOGLE_SHEET"]
 
 engine = create_engine(DATABASE_URL)
+
+df_gs = pd.read_csv(SHEETS_URL)
+print(df_gs)
+
+df_gs.to_sql("test_daily_waste", engine, if_exists="append", index=False)
 
 with engine.connect() as conn:
     df = pd.read_sql(text("""
         SELECT w.*, p.category, p.core
-        FROM daily_waste w
+        FROM test_daily_waste w
         LEFT JOIN pastries p ON w.name = p.name
+        ORDER BY date ASC
     """), conn)
 
-df["date"] = pd.to_datetime(df["date"])
-df["day"] =  pd.Categorical(df["date"].dt.day_name(), categories=days_order, ordered=True)
-df["week_num"] = (df["date"] - df["date"].min()).dt.days //7 + 1
+df['date'] = pd.to_datetime(df['date'])
+df['day'] =  pd.Categorical(df['date'].dt.day_name(), categories=days_order, ordered=True)
+df['week_num'] = (df['date'] - df['date'].min()).dt.days //7 + 1
+
+avg_day = (
+    df.groupby(["date", "day"])["waste"].sum()
+    .reset_index()
+    .groupby("day")["waste"].mean()
+    .reset_index()
+)
+print(avg_day)
+
+with engine.begin() as conn:
+    for _, row in avg_day.iterrows():
+        conn.execute(text("""
+            UPDATE avg_day_test
+            SET waste = :waste
+            WHERE day = :day
+            """), {"day": row.day, "waste": row.waste}
+        )
 
 print(df)
