2e85defef974df25f789f27eb7d4f1f818ebf0a3
Chooben <benmchoo@gmail.com>
2026-07-02 14:41:13 -0400

    Comment out local env import

--- scripts/script.py
+++ scripts/script.py
@@ -1,11 +1,11 @@
 import os
-from dotenv import load_dotenv
+#from dotenv import load_dotenv
 import pandas as pd
 from sqlalchemy import create_engine, text
 
 days_order = ["Monday", "Tuesday", "Wednesday", "Thursday","Friday", "Saturday", "Sunday"]
 
-load_dotenv()
+#load_dotenv()
 
 DATABASE_URL = os.environ["DATABASE_URL"]
 SHEETS_URL = os.environ["GOOGLE_SHEET"]
